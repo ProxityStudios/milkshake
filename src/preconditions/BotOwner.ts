9b1c0d720244142913c4588a4cb40fc5cd66ca18
Crawl <103999185+CrawlTheDev@users.noreply.github.com>
2022-04-26 01:41:31 +0300

    Update BotOwner.ts

--- src/preconditions/BotOwner.ts
+++ src/preconditions/BotOwner.ts
@@ -2,9 +2,9 @@ import { Precondition } from '@sapphire/framework';
 import type { Message } from 'discord.js';
 
 export class UserPrecondition extends Precondition {
-	public async run(message: Message) {
+	async run(message: Message) {
 		return this.container.config.client.ownerIDs.includes(message.author.id)
 			? this.ok()
-			: this.error({ message: 'This command can only be used by the bot owner.' });
-	}
+			: this.error({ message: 'This command can only be used by the bot owner(s).' });
+        }
 }
